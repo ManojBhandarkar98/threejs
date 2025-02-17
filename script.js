492b87a14d4443cc96499f7f2a1739aced7fefc2
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-02-17 20:22:37 +0530

    run camera, mesh, cube on screen

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector('.webgl');
+console.log(canvas);
+const scene = new THREE.Scene();
+
+
+const geomatry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({
+    color: 'red'
+}); 
+const mesh = new THREE.Mesh(geomatry, material);
+scene.add(mesh);
+
+//camera
+const sizes={
+    width:800,
+    height:600
+}
+const camera= new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+camera.position.z=3;
+
+//render
+const renderer = new THREE.WebGLRenderer( { canvas:canvas } );
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
